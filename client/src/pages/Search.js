f5c16db243cf4fb23960a3fcc4fe8d0621df7311
Robert Finkley <rfinkley82@gmail.com>
2020-05-04 16:13:14 -0400

    Upated Search page

---
+++ client/src/pages/Search.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+// import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { Input, FormBtn } from "../components/SearchForm";
+
+class Books extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+//   componentDidMount() {
+//     this.loadBooks();
+//   }
+
+//   loadBooks = () => {
+//     API.getBooks()
+//       .then(res =>
+//         this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+//       )
+//       .catch(err => console.log(err));
+//   };
+
+//   deleteBook = id => {
+//     API.deleteBook(id)
+//       .then(res => this.loadBooks())
+//       .catch(err => console.log(err));
+//   };
+
+//   handleInputChange = event => {
+//     const { name, value } = event.target;
+//     this.setState({
+//       [name]: value
+//     });
+//   };
+
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.title && this.state.author) {
+//       API.saveBook({
+//         title: this.state.title,
+//         author: this.state.author,
+//         synopsis: this.state.synopsis
+//       })
+//         .then(res => this.loadBooks())
+//         .catch(err => console.log(err));
+//     }
+//   };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="sm-12">
+            <Jumbotron>
+              <h1>(React) Google Books Search</h1>
+              <p>Search for and Save Books of Interest</p>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="sm-12">
+            <Container fluid>
+              <h2>Book Search</h2>
+              <Input />
+              <FormBtn>Search</FormBtn>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
