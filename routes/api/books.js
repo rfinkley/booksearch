26567c16c22a6a21ad9178d097422282e7c2726b
Robert Finkley <rfinkley82@gmail.com>
2020-05-05 16:40:15 -0400

    Updated routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -6,4 +6,9 @@ router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
 
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .delete(booksController.remove);
+
 module.exports = router;
