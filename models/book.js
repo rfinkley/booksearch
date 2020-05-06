50f37ae0731c7f89a423ce39e809a63797d92010
Robert Finkley <rfinkley82@gmail.com>
2020-05-05 21:08:22 -0400

    Updated book model

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   subtitle: String,
-  author: { type: String, required: true },
+  authors: { type: String, required: true },
   description: String,
   preview: String,
   image: { type: String, required: false }
