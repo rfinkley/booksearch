0c3c1052a8018b36fc8e50a381e3a38dd2d85a3a
Robert Finkley <rfinkley82@gmail.com>
2020-05-05 21:29:04 -0400

    Finished Saved page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
-// import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import Nav from "../components/Nav";
+import API from "../utils/API"
+import { FormBtnDelete ,FormBtnView, BookCard} from "../components/BookCard"
 
 class Books extends Component {
   state = {
@@ -12,43 +12,23 @@ class Books extends Component {
     synopsis: ""
   };
 
-//   componentDidMount() {
-//     this.loadBooks();
-//   }
+  componentDidMount() {
+    this.loadBooks();
+  }
 
-//   loadBooks = () => {
-//     API.getBooks()
-//       .then(res =>
-//         this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-//       )
-//       .catch(err => console.log(err));
-//   };
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data})
+      )
+      .catch(err => console.log(err));
+  };
 
-//   deleteBook = id => {
-//     API.deleteBook(id)
-//       .then(res => this.loadBooks())
-//       .catch(err => console.log(err));
-//   };
-
-//   handleInputChange = event => {
-//     const { name, value } = event.target;
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     if (this.state.title && this.state.author) {
-//       API.saveBook({
-//         title: this.state.title,
-//         author: this.state.author,
-//         synopsis: this.state.synopsis
-//       })
-//         .then(res => this.loadBooks())
-//         .catch(err => console.log(err));
-//     }
-//   };
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
@@ -59,6 +39,34 @@ class Books extends Component {
               <h1>(React) Google Books Search</h1>
               <p>Search for and Save Books of Interest</p>
             </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="sm-12">
+            <Container fluid>
+              <h2>Saved Books</h2>
+              {this.state.books.map( (book, index) => (
+                <BookCard key={index} className="container my-4">
+                  <div className="row">
+                    <div className="col">
+                      <h2>{book.title}</h2>
+                      <p>Subtitle: {book.subtitle ? book.subtitle : "No Subtitle"}  </p>
+                      <p>Author(s): {book.authors} </p>
+                      <FormBtnDelete onClick={() => this.deleteBook(book._id)} >Delete</FormBtnDelete>
+                      <FormBtnView href={book.preview} >View</FormBtnView>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-2">
+                      <img src={book.image} alt="Book Cover" className="img-thumbnail" />
+                    </div>
+                    <div className="col-10">
+                      <p>Description: {book.description}</p>
+                    </div>
+                  </div>
+                </BookCard>
+              ))}
+            </Container>
           </Col>
         </Row>
       </Container>
