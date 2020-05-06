9440c0547376db6187428866d20795abf0e0bf7b
Robert Finkley <rfinkley82@gmail.com>
2020-05-05 21:09:55 -0400

    Updated BookCard component

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
 
-export function FormBtn_Save(props) {
+export function FormBtnSave(props) {
     return (
-      <a {...props} className="btn btn-success bookCard-btn-save">
+      <button {...props} className="btn btn-success bookCard-btn-save">
         {props.children}
-      </a>
+      </button>
     );
 }
 
-export function FormBtn_View(props) {
+export function FormBtnView(props) {
     return (
       <a {...props} href={props.href} className="btn btn-success bookCard-btn-view">
         {props.children}
@@ -18,29 +17,10 @@ export function FormBtn_View(props) {
     );
 }
 
-function BookCard(props) {
-
+export function BookCard(props) {
   return (
     <div className="container my-4">
-        <div className="row">
-            <div className="col">
-                <h2>{props.title}</h2>
-                <p>Subtitle: {props.subtitle}</p>
-                <p>Author(s): {props.author}</p>
-                <FormBtn_View href={props.preview} >View</FormBtn_View>
-                <FormBtn_Save >Save</FormBtn_Save>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-2">
-                <img src={props.image} alt="Book Cover" className="img-thumbnail" />
-            </div>
-            <div className="col-10">
-                <p>Description: {props.description}</p>
-            </div>
-        </div>
+      {props.children}
     </div>
   );
 }
-
-export default BookCard;
